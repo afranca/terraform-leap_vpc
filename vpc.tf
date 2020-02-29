d1c845b67338712b509d539814c2f54a130e3c44
Alexandre <basic4@gmail.com>
2020-02-29 11:17:18 +0000

    Adding Route Table

--- vpc.tf
+++ vpc.tf
@@ -29,3 +29,16 @@ resource "aws_internet_gateway" "main-igw" {
     Name ="Main IGW"
   }
 }
+
+# Route Tables
+resource "aws_route_table" "main-public-rt" {
+  vpc_id  = aws.vpc.main.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-igw.id
+
+    tags = {
+      Name = "main-public-rt"
+    }
+  }
+}
