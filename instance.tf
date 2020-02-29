679f584d42837430c21911b36084bbbcdf41ed71
Alexandre <basic4@gmail.com>
2020-02-29 11:06:34 +0000

    Adding Security Group to EC2 | Creating public subnet

--- instance.tf
+++ instance.tf
@@ -4,4 +4,11 @@ resource "aws_instance" "example" {
 
   # the public SSH key
   key_name = aws_key_pair.leapkeypair.key_name  
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+ # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh-sg.id]  
+
 }
