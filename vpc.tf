91f75a7cd9eb8b9a86b804e8696bca1a13329e54
Alexandre <basic4@gmail.com>
2020-02-29 11:29:55 +0000

    Adding Route Table and Route Table Association with Subnet

--- vpc.tf
+++ vpc.tf
@@ -32,13 +32,19 @@ resource "aws_internet_gateway" "main-igw" {
 
 # Route Tables
 resource "aws_route_table" "main-public-rt" {
-  vpc_id  = aws.vpc.main.id
-  route = {
+  vpc_id  = aws_vpc.main.id
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main-igw.id
-
-    tags = {
-      Name = "main-public-rt"
-    }
   }
+  tags = {
+      Name = "main-public-rt"
+   }
 }
+
+# Route Associations Public
+resource "aws_route_table_association" "main-public-rta" {
+  subnet_id       = aws_subnet.main-public-1.id
+  route_table_id  = aws_route_table.main-public-rt.id
+}
+
