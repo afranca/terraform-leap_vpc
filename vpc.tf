a19f3c9a823b4b0ae0c5bce2cc5c4a604be824ad
Alexandre <basic4@gmail.com>
2020-03-04 14:17:56 +0000

    Adding Terraform VPC modules and Interpolation in Instance

--- vpc.tf
+++ vpc.tf
@@ -1,71 +1,40 @@
-# Internet VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
-  tags = {
-    Name = "main"
-  }
-}
+module "vpc-prod" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.5.0"
 
-# Public Subnets
-resource "aws_subnet" "main-public-sb" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-central-1a"
-  tags  = {
-    Name  = "main-public-sb"
-  }
-}
+  name = "vpc-prod"
+  cidr = "10.0.0.0/16"
 
-# Private Subnets
-resource "aws_subnet" "main-private-sb" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-central-1a"
+  azs             = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   tags = {
-    Name = "main-private-sb"
+    Terraform   = "true"
+    Environment = "prod"
   }
 }
-resource "aws_subnet" "main-private-sb-02" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-central-1b"
+
+module "vpc-dev" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.5.0"
+
+  name = "vpc-dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   tags = {
-    Name = "main-private-sb-02"
+    Terraform   = "true"
+    Environment = "dev"
   }
-}
-
-# Internet GW
-resource "aws_internet_gateway" "main-igw" {
-  vpc_id  = aws_vpc.main.id
-  tags = {
-    Name ="Main IGW"
-  }
-}
-
-# Route Tables
-resource "aws_route_table" "main-public-rt" {
-  vpc_id  = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-igw.id
-  }
-  tags = {
-      Name = "main-public-rt"
-   }
-}
-
-# Route Associations Public
-resource "aws_route_table_association" "main-public-rta" {
-  subnet_id       = aws_subnet.main-public-sb.id
-  route_table_id  = aws_route_table.main-public-rt.id
 }
 
